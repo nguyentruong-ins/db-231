5613d454cd19db41a09baebf29e5fa9746c4f6e2
Nguyen Truong <nguyen.truong@c0x12c.com>
2023-12-01 01:40:01 +0700

    - Minor changes

--- 1_2_1_Procedure.sql
+++ 1_2_1_Procedure.sql
@@ -105,18 +105,18 @@ END
 -- TEST SQL ---
 
 EXEC InsertNewEmployeeAccount 
-@username = 'nguyen.truong6', 
+@username = 'nguyen.truong1', 
 @password = '123456',
 @salary = 10000000,
 @dob = '2002/10/11',
 @employee_type = 'FULLTIME',
 @account_type = 'EMPLOYEE',
-@number = '0945430559',
+@number = '0945430551',
 @first_name = 'Nguyen',
 @last_name = 'Truong',
 @sex = 'MALE',
 @started_date = '2020/11/20',
-@ssn = '0870001',
+@ssn = '0870002',
 @address = 'Dorm A',
 @store_id = 1;
 
@@ -126,30 +126,30 @@ EXEC InsertNewEmployeeAccount
 
 CREATE OR ALTER PROCEDURE UpdateEmployeeAccount
 @username varchar(255),
-@password varchar(255) = NULL,
-@salary money = NULL,
-@dob datetime2 = NULL,
-@employee_type varchar(20) = NULL,
-@account_type varchar(100) = NULL,
+@new_password varchar(255) = NULL,
+@new_salary money = NULL,
+@new_dob datetime2 = NULL,
+@new_employee_type varchar(20) = NULL,
+@new_account_type varchar(100) = NULL,
 @old_number varchar(10) = NULL,
 @new_number varchar(10) = NULL,
-@first_name varchar(255) = NULL,
-@last_name varchar(255) = NULL,
-@address varchar(255) = NULL,
-@sex varchar(20) = NULL,
-@started_date datetime2 = NULL,
-@ssn varchar(9) = NULL,
-@store_id int = NULL,
-@status bit = NULL,
+@new_first_name varchar(255) = NULL,
+@new_last_name varchar(255) = NULL,
+@new_address varchar(255) = NULL,
+@new_sex varchar(20) = NULL,
+@new_started_date datetime2 = NULL,
+@new_ssn varchar(9) = NULL,
+@new_store_id int = NULL,
+@new_status bit = NULL,
 @age int = 0,
 @count int = 0,
 @isError bit = 0
 AS 
 BEGIN 
 	-- Check age
-	if @dob IS NOT NULL 
+	if @new_dob IS NOT NULL 
 	BEGIN 
-		SELECT @age=(CONVERT(int,CONVERT(char(8),GETDATE(),112)) - (CONVERT(char(8), @dob, 112)))/10000;
+		SELECT @age=(CONVERT(int,CONVERT(char(8),GETDATE(),112)) - (CONVERT(char(8), @new_dob, 112)))/10000;
 		if @age < 18
 		BEGIN
 			SET @isError = 1;			
@@ -159,8 +159,8 @@ BEGIN
 	END
 	
 	-- Check acocunt type
-	if @employee_type IS NOT NULL
-		if not (@employee_type = 'PARTTIME' or @employee_type = 'FULLTIME')
+	if @new_employee_type IS NOT NULL
+		if not (@new_employee_type = 'PARTTIME' or @new_employee_type = 'FULLTIME')
 		BEGIN 
 			SET @isError = 1;
 			RAISERROR ('Employee type must be SHIPPER or MANAGER or EMPLOYEE', 16, 1);			
@@ -168,8 +168,8 @@ BEGIN
 		
 	
 	-- Check employee type
-	if @account_type IS NOT NULL
-		if not (@account_type = 'SHIPPER' or @account_type = 'MANAGER' or @account_type = 'EMPLOYEE')
+	if @new_account_type IS NOT NULL
+		if not (@new_account_type = 'SHIPPER' or @new_account_type = 'MANAGER' or @new_account_type = 'EMPLOYEE')
 		BEGIN 
 			SET @isError = 1;
 			RAISERROR ('Account type must be SHIPPER or MANAGER or EMPLOYEE', 16, 1);			
@@ -177,8 +177,8 @@ BEGIN
 		
 		
 	-- Check sex type
-	if @sex IS NOT NULL
-		if not (@sex = 'MALE' or @sex = 'FEMALE' or @sex = 'OTHER')
+	if @new_sex IS NOT NULL
+		if not (@new_sex = 'MALE' or @new_sex = 'FEMALE' or @new_sex = 'OTHER')
 		BEGIN 
 			SET @isError = 1;			
 			RAISERROR ('Sex must be MALE, FEMALE or OTHER', 16, 1);
@@ -186,28 +186,40 @@ BEGIN
 		
 	
 	-- Check started date
-	if @started_date IS NOT NULL
-		if @started_date > GETDATE()
+	if @new_started_date IS NOT NULL
+		if @new_started_date > GETDATE()
 		BEGIN
 			SET @isError = 1;			
 			raiserror ('Started date must before current date', 16, 1);
 		END
 		
 	
-	if @ssn IS NOT NULL
+	if @new_ssn IS NOT NULL
 	BEGIN
-		SELECT @count = COUNT(*) from employee_accounts where ssn = @ssn and username != @username;
+		SELECT @count = COUNT(*) from employee_accounts where ssn = @new_ssn and username != @username;
 		if @count > 0
 		BEGIN
 			SET @isError = 1;			
-			RAISERROR ('Duplicated ssn', 16, 1);	
+			RAISERROR ('New ssn is duplicated', 16, 1);	
+		END
+		
+	END
+		
+	
+	if @new_number IS NOT NULL
+	BEGIN
+		SELECT @count = COUNT(*) from employee_numbers where number = @new_number and employee_username != @username;
+		if @count > 0
+		BEGIN
+			SET @isError = 1;			
+			RAISERROR ('New number is duplicated', 16, 1);	
 		END
 		
 	END
 	
-	if @store_id IS NOT NULL
+	if @new_store_id IS NOT NULL
 	BEGIN		
-		SELECT @count = COUNT(*) from stores where id=@store_id 
+		SELECT @count = COUNT(*) from stores where id=@new_store_id 
 		if @count = 0 
 		BEGIN 
 			SET @isError = 1;			
@@ -226,18 +238,18 @@ BEGIN
 	if @isError != 1
 	BEGIN		
 		UPDATE employee_accounts SET 
-			salary = ISNULL(@salary, salary),
-			address = ISNULL(@address, address),
-			dob = ISNULL(@dob, dob),
-			account_type = ISNULL(@account_type, account_type),
-			last_name = ISNULL(@last_name, last_name),
-			first_name = ISNULL(@first_name, first_name),
-			sex = ISNULL(@sex, sex),
-			started_date = ISNULL(@started_date, started_date),
-			ssn = ISNULL(@ssn, ssn),
-			employee_type = ISNULL(@employee_type, employee_type),
-			store_id = ISNULL(@store_id, store_id),
-			status = ISNULL(@status, status)
+			salary = ISNULL(@new_salary, salary),
+			address = ISNULL(@new_address, address),
+			dob = ISNULL(@new_dob, dob),
+			account_type = ISNULL(@new_account_type, account_type),
+			last_name = ISNULL(@new_last_name, last_name),
+			first_name = ISNULL(@new_first_name, first_name),
+			sex = ISNULL(@new_sex, sex),
+			started_date = ISNULL(@new_started_date, started_date),
+			ssn = ISNULL(@new_ssn, ssn),
+			employee_type = ISNULL(@new_employee_type, employee_type),
+			store_id = ISNULL(@new_store_id, store_id),
+			status = ISNULL(@new_status, status)
 		WHERE username = @username
 		
 		UPDATE employee_numbers SET 
@@ -245,7 +257,7 @@ BEGIN
 		WHERE number = @old_number AND employee_username = @username
 		
 		UPDATE accounts SET 
-			password = ISNULL(@password, password)
+			password = ISNULL(@new_password, password)
 		WHERE username = @username
 	END
 END
@@ -253,21 +265,21 @@ END
 -- TEST SQL --
 
 EXEC UpdateEmployeeAccount
-@username = 'nguyen.truong5', 
-@password = '1234569999',
-@salary = 1999999,
-@dob = '2003/11/12',
-@employee_type = 'FULLTIME',
-@account_type = 'EMPLOYEE',
-@old_number = '0945430555',
+@username = 'nguyen.truong', 
+@new_password = '1234569999',
+@new_salary = 1999999,
+@new_dob = '2003/11/12',
+@new_employee_type = 'FULLTIME',
+@new_account_type = 'EMPLOYEE',
+@old_number = '0945430559',
 @new_number = '0945430444',
-@first_name = 'Nguyen',
-@last_name = 'Truong',
-@sex = 'MALE',
-@started_date = '2020/11/20',
-@ssn = '0870002',
-@address = 'Dorm A',
-@store_id = 1;
+@new_first_name = 'Nguyen',
+@new_last_name = 'Truong',
+@new_sex = 'MALE',
+@new_started_date = '2020/11/20',
+@new_ssn = '0870003',
+@new_address = 'Dorm A',
+@new_store_id = 1;
 
 ------------------------------------------------
 
