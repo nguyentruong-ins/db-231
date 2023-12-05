f17705da175f440f8c51de0964811f8b8e1c730b
Leino Vieira <cuong.lejoseph@hcmut.edu.vn>
2023-12-06 00:38:27 +0700

    Update discount_amount_by_membership.sql
    Update directly_order test, still stable

--- discount_amount_by_membership.sql
+++ discount_amount_by_membership.sql
@@ -36,6 +36,7 @@ INSERT INTO accounts (username, password) values ('customer6', 'password6');
 INSERT INTO customer_accounts (username, total_points, account_status, latest_ordered_at, customer_id) values ('customer5', 50, 'ACTIVE', convert(datetime, '20/11/2023', 103), 11);
 INSERT INTO customer_accounts (username, total_points, account_status, latest_ordered_at, customer_id) values ('customer6', 100, 'INACTIVE', convert(datetime, '01/01/2017', 103), 12);
 
+--Online orders
 INSERT INTO online_orders 	(order_id, status, shipping_fee, shipping_address, predicted_arrived_time, customer_account_username, export_employee_username, shipping_employee_username, discount_amount_by_point)
 				VALUES		(10, 'DONE', 0, '...', '11-10-2023 10:00', 'customer5', 'employee1', 'employee3', 0);
 INSERT INTO online_orders 	(order_id, status, shipping_fee, shipping_address, predicted_arrived_time, customer_account_username, export_employee_username, shipping_employee_username, discount_amount_by_point)
@@ -44,7 +45,7 @@ INSERT INTO online_orders 	(order_id, status, shipping_fee, shipping_address, pr
 INSERT INTO order_items (order_id, item_id, actual_price, quantity)       --Memebership customer5: 10%
 				VALUES		(10, 20, 50000, 4);
 INSERT INTO order_items (order_id, item_id, actual_price, quantity)
-				VALUES		(10, 21, 50000, 1);
+				VALUES		(10, 21, 50000, 2);
 INSERT INTO order_items (order_id, item_id, actual_price, quantity)		--Memebership customer6: 25%
 				VALUES		(11, 10, 45000, 1);
 INSERT INTO order_items (order_id, item_id, actual_price, quantity)
@@ -53,3 +54,23 @@ INSERT INTO order_items (order_id, item_id, actual_price, quantity)
 				VALUES		(11, 12, 32500, 2);
 
 DELETE FROM order_items WHERE order_id = 10 AND item_id = 20
+
+--Offline orders
+INSERT INTO orders (total_amount, discount_amount_by_voucher, discount_amount_by_membership, ordered_at) values (0, 0, 0, '11-10-2023');
+INSERT INTO orders (total_amount, discount_amount_by_voucher, discount_amount_by_membership, ordered_at) values (0, 0, 0, '11-15-2023');
+
+INSERT INTO directly_orders (order_id, change, customer_payment, customer_id, export_emp_username) 
+				VALUES		(12, 0, 200000, 11, 'employee5');
+INSERT INTO directly_orders (order_id, change, customer_payment, customer_id, export_emp_username) 
+				VALUES		(13, 0, 50000, 12, 'employee6');
+
+INSERT INTO order_items (order_id, item_id, actual_price, quantity)       --Memebership customer5: 10%
+				VALUES		(12, 20, 50000, 4);
+INSERT INTO order_items (order_id, item_id, actual_price, quantity)
+				VALUES		(12, 21, 50000, 2);
+INSERT INTO order_items (order_id, item_id, actual_price, quantity)		--Memebership customer6: 25%
+				VALUES		(13, 10, 45000, 1);
+INSERT INTO order_items (order_id, item_id, actual_price, quantity)
+				VALUES		(13, 11, 45000, 2);
+INSERT INTO order_items (order_id, item_id, actual_price, quantity)
+				VALUES		(13, 12, 32500, 2);
