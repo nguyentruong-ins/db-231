78a96a43ea0b6bbbe96f77934d71a7f8e802abc1
Bâng Đỗ Văn <bang.dovan38@gmail.com>
2023-12-11 19:01:17 +0700

    update proc top customers

--- 1.2.3/proc_top_customers.sql
+++ 1.2.3/proc_top_customers.sql
@@ -13,20 +13,39 @@ BEGIN
             SUM(o.total_amount) AS total_order_amount
         FROM
             customers c
-            JOIN customer_accounts ca ON c.id = ca.customer_id
-            LEFT JOIN directly_orders do ON c.id = do.customer_id
-            LEFT JOIN online_orders oo ON (ca.username = oo.customer_account_username)
-            LEFT JOIN orders o ON do.order_id = o.id OR oo.order_id = o.id
+            JOIN directly_orders do ON c.id = do.customer_id
+            JOIN orders o ON do.order_id = o.id
         WHERE
             o.ordered_at >= @startDate AND o.ordered_at <= @endDate
         GROUP BY
             c.id, c.first_name, c.last_name, c.phone_number
-        ORDER BY
-            total_order_amount DESC
+
+        UNION ALL
+
+        SELECT TOP (@numberOfCustomers)
+            c.id AS customer_id,
+            c.first_name,
+            c.last_name,
+            c.phone_number,
+            SUM(o.total_amount) AS total_order_amount
+        FROM
+            customers c
+            JOIN customer_accounts ca ON c.id = ca.customer_id
+            JOIN online_orders oo ON (ca.username = oo.customer_account_username)
+            JOIN orders o ON oo.order_id = o.id
+        WHERE
+            o.ordered_at >= @startDate AND o.ordered_at <= @endDate
+        GROUP BY
+            c.id, c.first_name, c.last_name, c.phone_number
     )
 
-    SELECT *
-    FROM OrderedCustomers;
+    SELECT 
+		customer_id,
+		first_name,
+		last_name,
+		phone_number,
+		SUM(total_order_amount) AS total_order_amount
+    FROM OrderedCustomers
+	GROUP BY customer_id, first_name, last_name, phone_number
+    ORDER BY total_order_amount DESC;
 END;
-
--- EXEC GetTopCustomersWithLargestOrders '2023-01-01', '2023-12-31', 10;
